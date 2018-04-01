d9eea8b1ad031e8c0709e6f66a38aec33f81f565
TheSamLloyd <samlloydj@gmail.com>
2018-04-01 14:07:08 -0400

    tried to take github shortcuts and ended up not adding the router files.. classique.

---
+++ api-router.js
@@ -0,0 +1,24 @@
+//requirements
+const express=require("express");
+const request = require('request-promise');
+const router = express.Router();
+	function response(url,req,res){
+		const accessToken = res.app.get('token')
+		const shopRequestHeaders = {
+        'X-Shopify-Access-Token': accessToken,
+      };
+		request({method:req.method, url:"https://societe-portsmouth.myshopify.com/admin/"+url, body:req.body, headers : shopRequestHeaders})
+		.then((shopResponse)=>{
+			res.status(200).end(shopResponse);
+		})
+	}
+	router.get("/info", (req,res)=>{
+		response("shop.json", req, res);
+	})
+	router.get("/products", (req,res)=>{
+		response("products.json", req, res)
+	})
+	router.post("/products", (req,res)=>{
+		response("products.json", req, res)
+	})
+module.exports=router;
