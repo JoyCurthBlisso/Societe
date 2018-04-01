8b6c77329eb40a37c27cb2139437caf3d4ed4dd2
TheSamLloyd <samlloydj@gmail.com>
2018-04-01 13:58:45 -0400

    added some API routes. the post route technically works but you can't access it thru the browser.

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser")
 const PORT = (process.env.PORT || 80);
-app.use(require("./router.js"));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use("/api", require("./api-router.js"))
+app.use("/",require("./router.js"));
 app.listen(PORT, () => {
   console.log('Example app listening on port '+PORT);
 });
