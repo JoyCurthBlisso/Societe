259ecf985ec5e7095c5b04b396bfb0fcb9e2b25b
scull1916 <kpscully@gmail.com>
2018-04-02 20:08:45 -0400

    initial schema & seeds for db

---
+++ seeds.sql
@@ -0,0 +1,35 @@
+INSERT INTO t_Material (commonName, cost, lot, purchaseDate) VALUES
+('Acacia Powder', '12.5', '23426', '2017-11-16'),
+('Activated Charcoal', '30.4', '73395', '2017-11-16'),
+('Activated Charcoal', '30.4', '73647', '2017-11-16'),
+('Alfalfa Leaf', '11', '23497', '2017-11-16'),
+('Anise Star Pods, Whole', '15', '23563', '2017-11-16'),
+('Anise Star', '22.08', '54987', '2017-11-16'),
+('Arrow Root Powder', '40', 'B8856', '2017-11-16');
+
+INSERT INTO t_Location (location) VALUES
+('Jar'),
+('Basement'),
+('Cabinet'),
+('Teabox');
+
+INSERT INTO t_Sourcing (sourcing) VALUES
+('Organic'),
+('Wild Harvested'),
+('N/A');
+
+INSERT INTO t_Channel (channel) VALUES
+('Amazon'),
+('Bliss'),
+('Joy Curth web'),
+('2Joes');
+
+INSERT INTO t_UoM (measure) VALUES
+('lb'),
+('oz'),
+('ml');
+
+INSERT INTO t_Suppliers (name, contact, address1, address2, city, state, postCode, country, phone1, phone2, email1, email2, url) VALUES
+('Bluechai', 'Stefan Dorn', '', '', 'Horgenzell', '', '88263', 'Germany', '', '', 'hi@bluechai.com', '', 'http://bluechai.com'),
+('Mountain Rose Herbs', '', 'PO Box 50220', '', 'Eugene', 'OR', '97405', 'USA', '800-879-3337', '541-741-7307', 'support@mountainroseherbs.com', '', 'http://mountainroseherbs.com');
+
