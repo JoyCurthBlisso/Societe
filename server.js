123801116beb029a206e54bea0e9d863b0f25a15
scull1916 <kpscully@gmail.com>
2018-04-04 12:51:59 -0400

    start bulding models

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 var db = require("./models");
 const app = express();
 const bodyParser = require("body-parser")
-const PORT = (process.env.PORT || 3000);
+const PORT = (process.env.PORT || 8080);
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use("/api", require("./routes/api-router.js"))
