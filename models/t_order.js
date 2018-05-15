0939e8bf9ba5abf7f46050127c9f1c44fd0e4cbf
Keira Nevers <keira.nevers@gmail.com>
2018-05-14 20:50:36 -0400

    new model files

---
+++ models/t_order.js
@@ -0,0 +1,35 @@
+module.exports = function(sequelize, DataTypes)
+{
+	var Order = sequelize.define("Order",
+	{
+		order_id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true
+		},
+		purchase_order_item: {
+			type: DataTypes.STRING
+		},
+		vendorName: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		order_date: {
+			type: DataTypes.DATEONLY,
+			allowNull: false
+		},
+		shipping_amt: {
+			type: DataTypes.DECIMAL
+		},
+		total_amount: {
+			type: DataTypes.DECIMAL
+		}
+	});
+	// Order.associate = function(models) {
+ //        Order.hasMany(models.Order_item, {
+ //            onDelete: "cascade"
+ //        });
+ //    };
+
+	return Order;
+};
