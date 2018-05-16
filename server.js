69390444f87d40a1e805db7ab942f71e633055a1
Joy Curth <joy.curth@comcast.net>
2018-05-16 19:47:40 -0400

    updated with react

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require('express');
-var db = require("./models");
-const app = express();
-const bodyParser = require("body-parser")
+const path = require("path");
+const bodyParser = require("body-parser");
 const PORT = (process.env.PORT || 8080);
+const app = express();
+const apiRoutes = require("./routes/apiRoutes");
+
+var db = require("./models");
+
 app.use(bodyParser.urlencoded({extended:true}));
+
 app.use(bodyParser.json());
+
+app.use("/api", apiRoutes);
+
 app.use("/shopify", require("./routes/shopify-router.js"))
 app.use("/db", require("./routes/db-router.js"))
 app.use("/",require("./routes/router.js"));
 app.use("/", express.static(__dirname+"/public"))
+
+app.get("*", function(req, res){
+	res.sendFile(path.join(_dirname, "./client/build/index.html"));
+}
+
+app.listen(PORT, function(){
+	console.log('server now on port ${PORT}');
+}
+
 db.sequelize.sync().then(function()
 	{
 	app.listen(PORT, () => {
