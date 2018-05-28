68f8e414b1d4aff47d61d2aea11e59165480e26a
Joy Curth <joy.curth@comcast.net>
2018-05-27 23:33:38 -0400

    updated react components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import CustomerCard from "./components/CustomerCard";
 import logo from './logo.svg';
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./customer.json";
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
+      <Wrapper>
+      <Title>Customer List</Title>
+      {this.state.customer.map(customer => (
+        <CustomerCard
+          removeCustomer={this.removeCustomer}
+          id={Customer.id}
+          key={Customer.id}
+          name={Customer.name}
+          email={email.name}
+          patient={patient.name}
+        />
+      ))}
+    </Wrapper>
+
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
